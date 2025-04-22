fdc072591d1ad084e1fa4b3d8132f6e3821b9f42
Matang Patel <matangpatel2478@gmail.com>
2025-04-23 00:17:42 +0530

    Delete 9 directory

--- 9/5.py
+++
@@ -1,8 +0,0 @@
-def power(a, b):
-    if b == 0:
-        return 1
-    return a * power(a, b - 1)
-
-x = int(input("Enter base: "))
-y = int(input("Enter power: "))
-print(power(x, y))
--- 9/8.py
+++
@@ -1,7 +0,0 @@
-def string_len(s):
-    if s == "":
-        return 0
-    return 1 + string_len(s[1:])
-
-x = input("Enter a string: ")
-print(string_len(x))
